0bd8e5e856cd4a3f5c39a88871b0ecc9b5dbeecf
whyjyj <yoon_jae___z@naver.com>
2023-10-06 17:47:55 +0900

    basic settings

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
-export const SITE_NAME = "NimbusNarratives";
-export const SITE_TITLE = "NimbusNarratives | A personal blog by Abdullah Elsayed (@abdllahdev)";
+export const SITE_NAME = "Yoonjae Baek";
+export const SITE_TITLE = "Yoonjae Baek | A personal blog by Yoonjae Baek";
 export const SITE_DESCRIPTION =
-  "NimbusNarratives | A personal blog by Abdullah Elsayed (@abdllahdev)";
-export const TWITTER_HANDLE = "https://twitter.com/abdllah_dev";
-export const GITHUB_HANDLE = "https://github.com/abdllahdev";
+  "Yoonjae Baek | A personal blog by Yoonjae Baek (@whyj_yj)";
+export const TWITTER_HANDLE = "https://twitter.com";
+export const GITHUB_HANDLE = "https://github.com/whyjyj";
 export const CLOUDINARY_NAME = "example";
